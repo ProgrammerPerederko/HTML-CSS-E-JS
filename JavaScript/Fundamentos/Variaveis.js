ec46eaa23d776c999eb0919846cb083032e317a5
Daniel <perederkodaniel@gmail.com>
2026-08-01 17:19:59 -0300

    Subindo o arquivo de variaveis atualizado

--- JavaScript/Fundamentos/Variaveis.js
+++ JavaScript/Fundamentos/Variaveis.js
@@ -14,6 +14,7 @@ Regras para Declaração de Variáveis:
  - Não podem ser iniciadas com números, apenas com letras ou "_"
  - Não podem ser ultilizados caracteres especiais como "ç", "^", "~"
  - Não podem ser iguais as palavras reservados na linguagem
+ - Sempre  usar ; no final de cada comando
 
  Declarando variaveis com Let:
 
@@ -26,14 +27,23 @@ Regras para Declaração de Variáveis:
 
 
 // String
-let texto = "String"
+let texto = "String";
 
 // Number
-let numeroInteiro = 10
-let numeroFracionado = 11210.75
+let numeroInteiro = 10;
+let numeroFracionado = 11210.75;
 
 // boolean
-let Temdinheiro = false //true  -> verdadeiro / false -> falso 
+let Temdinheiro = false; //true  -> verdadeiro / false -> falso 
 
-// alert(numeroFracionado) // abrir um dialog
-document.write(texto)
+//null
+let valorNull = null;
+
+//undefined
+let valorUndefined;
+
+// Alterado valores de variaveis
+
+valorUndefined = "Valor"
+
+console.log(valorUndefined)
