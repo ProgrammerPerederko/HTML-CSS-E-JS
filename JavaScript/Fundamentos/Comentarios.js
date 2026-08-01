448fd5ec95361f4f00e8b8b1f00f315f1b2fb73a
Daniel <perederkodaniel@gmail.com>
2026-08-01 16:31:30 -0300

    Subindo os Fundamentos do JS

---
+++ JavaScript/Fundamentos/Comentarios.js
@@ -0,0 +1,10 @@
+// Tipos de Comentario
+
+// comentario de uma linha
+
+/*
+Comentario de múltiplas linhas
+Esse tipo de comentário permite 
+que várias linhas sejam encapsuladas 
+Em um mesmo comentario
+*/ 
---
+++ JavaScript/Fundamentos/Variaveis.js
@@ -0,0 +1,39 @@
+// Variaveis - Introdução
+
+/* 
+O que variaveis?
+
+Tipos de variáveis
+
+- String - Texto Caracteres
+- Number - Inteiros 
+- Boolean - True ou False
+
+Regras para Declaração de Variáveis:
+
+ - Não podem ser iniciadas com números, apenas com letras ou "_"
+ - Não podem ser ultilizados caracteres especiais como "ç", "^", "~"
+ - Não podem ser iguais as palavras reservados na linguagem
+
+ Declarando variaveis com Let:
+
+ Case Sensitive:
+ 
+ let curso, let Curso, let CURSO.
+
+ Exemplos:
+*/
+
+
+// String
+let texto = "String"
+
+// Number
+let numeroInteiro = 10
+let numeroFracionado = 11210.75
+
+// boolean
+let Temdinheiro = false //true  -> verdadeiro / false -> falso 
+
+// alert(numeroFracionado) // abrir um dialog
+document.write(texto)
